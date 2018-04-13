fb4c26a4a45ebcc1a7427309bccda6c0d6ec9467
Lxhs <31303529+Lxhs@users.noreply.github.com>
2018-04-13 20:06:51 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <cn-Header></cn-Header>
+    <!--<router-view></router-view>-->
+    <router-view name='main'></router-view>
+    <router-view name='sidebar'></router-view>
+  </div>
+</template>
+
+<script>
+import cnHeader from './components/Header'
+export default {
+  name: 'App',
+  comments: {
+    cnHeader
+  },
+  components: {
+    cnHeader
+  }
+}
+</script>
+
+<style>
+  html {
+    background-color: #DDDDDD;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-size: 20px;
+  }
+  body,div,span,a,p,ul,li {
+    margin: 0;
+    padding: 0;
+  }
+</style>
